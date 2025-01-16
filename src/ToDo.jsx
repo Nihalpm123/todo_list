ec59132fa954abfce6f48686ad7593077c4f84d4
Nihal <nihal.pm9633@gmail.com>
2025-01-16 12:52:11 +0530

    new update

---
+++ src/ToDo.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './Todo.css';
+
+class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: '',
+            tasks: []
+        };
+    }
+
+    inputChange = (eve) => {
+        this.setState({ task: eve.target.value });
+    };
+
+    addTask = () => {
+        const { task, tasks } = this.state;
+        if (task.trim() === '') return;
+
+        this.setState({
+            tasks: [...tasks, task],
+            task: ''
+        });
+    };
+
+    render() {
+        return (
+            <div className="todo-app">
+                <h1>To-Do List</h1>
+                <div className="input-container">
+                    <input className='task'
+                        type="text"
+                        value={this.state.task}
+                        onChange={this.inputChange}
+                        placeholder="Enter your task"
+                    />
+                    <button onClick={this.addTask}>Add Task</button>
+                </div>
+                <ol className="task-list">
+                    {this.state.tasks.map((text, index) => (
+                        <li key={index}>{text}</li>
+                    ))}
+                </ol>
+            </div>
+        );
+    }
+}
+
+export default Todo;
